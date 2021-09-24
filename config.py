d98dd91e9c95cb8f26af7e3465aee99788586316
2KIRAT3 <hvheblanbi@gmail.com>
2021-09-24 15:04:34 +0800

    init

---
+++ config.py
@@ -0,0 +1,8 @@
+login = "Your Login"
+password = "Your Password"
+stremer = "NickName Streamer"
+load_cookies = False
+silent = False
+mute_audio = False
+auto_160p = False
+key_stop = 'end'
---
+++ main.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+import time,config,pickle
+
+from selenium.webdriver.chrome.options import Options
+import os.path
+import keyboard
+options = webdriver.ChromeOptions()
+url = "https://www.twitch.tv/"+config.stremer
+options.headless = config.silent
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0")
+#mute
+if config.mute_audio:
+    options.add_argument("--mute-audio")
+path_to_driver = r"chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(path_to_driver,chrome_options=options)
+def log_to_terminal(text):
+    print(text)
+try:
+    driver.get(url)
+    time.sleep(2)
+    #authorization
+    if config.load_cookies == False:
+        login_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/nav/div/div[3]/div[3]/div/div[1]/div[1]/button").click()
+        time.sleep(5)
+        login_input = driver.find_element_by_id("login-username")
+        password_input = driver.find_element_by_id("password-input")
+        login_input.send_keys(config.login)
+        password_input.send_keys(config.password)
+        login1_input = driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[3]/form/div/div[3]/button").click()
+        time.sleep(30)
+        if os.path.isfile("cookies") == False:
+            pickle.dump(driver.get_cookies(),open("cookies",'wb'))
+        time.sleep(30)
+    else:
+        if os.path.isfile("cookies"):
+            for cookies in pickle.load(open("cookies",'rb')):
+                driver.add_cookie(cookies)
+    time.sleep(5)
+    driver.refresh()
+    log_to_terminal("Login successful")
+    time.sleep(10)
+    #18+ button
+    try:
+     driver.find_element_by_xpath("//button[@data-a-target='player-overlay-mature-accept']").click()
+    except:
+        pass
+    time.sleep(5)
+    #160p quality
+    if config.auto_160p:
+        driver.find_element_by_xpath("//button[@data-a-target='player-settings-button']").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("//button[@data-a-target='player-settings-menu-item-quality']").click()
+        time.sleep(1)
+        a = driver.find_elements_by_xpath("//div[@data-a-target='player-settings-submenu-quality-option']")
+        #a[0].click() #is auto
+        a[5].click()
+    while keyboard.is_pressed(config.key_stop)==False:
+     try:
+            driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div/div/section/div/div[5]/div[2]/div[2]/div[1]/div/div/div/div[2]/div/div/div/button").click()
+            time.sleep(5)
+            log_to_terminal("The reward was collected")
+     except:
+        pass
+except Exception:
+    print(Exception)
+finally:
+    driver.close()
+    driver.quit()
